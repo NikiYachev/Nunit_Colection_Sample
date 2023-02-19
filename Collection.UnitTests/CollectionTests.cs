d7d0697294185d38c934f5cb5497553d878f7940
NikiYachev <nikiyachev@gmail.com>
2023-02-19 16:41:07 +0200

    Add project files.

---
+++ Collection.UnitTests/CollectionTests.cs
@@ -0,0 +1,17 @@
+namespace Collection.UnitTests
+{
+    public class CollectionTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            Console.WriteLine("Shte stanat neshtata!");
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ Collection/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Collection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var collection = new Collection<int>();
+
+            Console.WriteLine("Current Collection: " + collection.ToString());
+
+            Console.WriteLine("Collection count: " + collection.Count);
+            Console.WriteLine("Collection capacity: " + collection.Capacity);
+
+            collection.Add(4);
+            Console.WriteLine("Current Collection: " + collection.ToString());
+
+            collection.AddRange(7, 12, 23, 45, 6);
+            Console.WriteLine("Current Collection: " + collection.ToString());
+
+            Console.WriteLine("Collection first 2 elements: " + collection[0] + " " + collection[1]);
+
+            collection[0] = 900;
+            Console.WriteLine("Current Collection: " + collection.ToString());
+
+            collection.InsertAt(3, 55);
+            Console.WriteLine("Current Collection: " + collection.ToString());
+
+            collection.Exchange(1, 4);
+            Console.WriteLine("Current Collection: " + collection.ToString());
+
+            collection.RemoveAt(2);
+            Console.WriteLine("Current Collection: " + collection.ToString());
+
+            collection.Clear();
+            Console.WriteLine("Current Collection: " + collection.ToString());
+
+        }
+    }
+}
